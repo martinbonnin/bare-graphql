842f0a55bcdef403f67c2d12e5b4924c150248a6
Martin Bonnin <martin@mbonnin.net>
2022-06-12 23:56:52 +0200

    version is now 0.0.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ sjmp {
         publication {
             artifactId = "bare-graphql"
             groupId = "net.mbonnin.bare-graphql"
-            version = "0.0.1"
+            version = "0.0.2-SNAPSHOT"
             simplePom {
                 name = "bare-graphql"
                 githubRepository = "martinbonnin/bare-graphql"
